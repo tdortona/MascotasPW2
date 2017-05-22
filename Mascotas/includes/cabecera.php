20aba679c994e92738c404481d8aa0eafacb45ae
Santiago Leiva <santiagoeleiva@gmail.com>
2017-05-21 23:09:07 -0300

    Agregado de Favicon

--- Mascotas/includes/cabecera.php
+++ Mascotas/includes/cabecera.php
@@ -4,6 +4,9 @@
 		<meta charset="UTF-8">
 		<title>PetFace - TP PWII</title>
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+		<link rel="icon" href="img/petface_icon.ico"/>
+
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/jquery-ui.min.css">
 		<link rel="stylesheet" href="css/jquery-ui.theme.min.css">
