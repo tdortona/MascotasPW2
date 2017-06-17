545c825ef7b29d3a281dd75766a1aeeeb8c6875f
Santiago Leiva <santiagoeleiva@gmail.com>
2017-06-16 21:08:24 -0300

    Login con Facebook

--- Petface/login-facebook.php
+++ Petface/login-facebook.php
@@ -4,18 +4,18 @@
 	session_start();
 
 	/* Cambiar según la ubicación de tu directorio*/
-	require_once __DIR__ . '/facebook/src/Facebook/autoload.php';
+	require_once __DIR__ . '/facebook-login/src/Facebook/autoload.php';
 
 	$fb = new Facebook\Facebook([
-		'app_id' => 'Su App ID',
-		'app_secret' => 'Su App Secret',
+		'app_id' => '456227338083821',
+		'app_secret' => '266219d785d79fbab525ce48a99d525e',
 		'default_graph_version' => 'v2.4',
 		]);
 
 	$helper = $fb->getRedirectLoginHelper();
 
 	$permissions = ['email']; // Permisos opcionales
-	$loginUrl = $helper->getLoginUrl('http://localhost/webs/ejemplos/facebook/fb-callback.php', $permissions);
+	$loginUrl = $helper->getLoginUrl('http://localhost/Petface/facebook-login/fb-callback.php', $permissions);
 
 	/* Link a la página de login*/
 	echo '<a href="' . htmlspecialchars($loginUrl) . '">Login con Facebook!</a>';
